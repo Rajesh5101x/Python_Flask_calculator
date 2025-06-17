534dcd04d79d165f66fa83259a0236ad5a96fd6c
Rajesh5101x <rr0656966@gmail.com>
2025-06-17 10:05:44 +0530

    updating many things

--- app.py
+++ app.py
@@ -15,5 +15,17 @@ def calc():
 def result(res):
     return render_template('result.html',title="Result",res=res)
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/features')
+def features():
+    return render_template('features.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
